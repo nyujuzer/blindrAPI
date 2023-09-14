372c1dc3c2c0016de6aacc8d3879be99aee06c80
nyujuzer <sallairobert94@gmail.com>
2023-09-14 09:24:08 +0200

    Deminek

--- blindr/blindr/settings.py
+++ blindr/blindr/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/static'
 STATICFILES_DIRS = path.join(BASE_DIR, 'static'),
 STATIC_ROOT = path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URL = path.join(STATIC_URL,'media/')
+MEDIA_URL = 'static/media'
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
